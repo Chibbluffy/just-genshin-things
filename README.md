43186b5d92f0035e3567d9484ff20cc3d5333b67
Chibbluffy <xian.tom.wan@gmail.com>
2022-02-18 11:21:14 -0600

    working beta version; need to add custom stats for some characters and fix amp reactions

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# just-genshin-things
-Random little scripts for like calculating genshin stats/damages/etc
+# Simple Comprehensive Genshin Damage Calculator
+
+## Sources
+- Genshin character stats and skill multipliers are from [HoneyImpact](https://genshin.honeyhunterworld.com/?lang=EN)
+- Genshin damage calculations are from the [wiki](https://genshin-impact.fandom.com/wiki/Genshin_Impact_Wiki)
+
+## Inputs
+- character name
+- character talent levels
+    - if talent does elemental damage
+        - Ex. if character is a catalyst, talent 1 should be dealing elemental damage, not physical damage
+        - Ex. Eula's talent 3 deals physical damage, not elemental damage
+- character total stats (from more details screen)
+    - hp
+    - atk
+    - def
+    - crit rate
+    - crit dmg
+    - elemental mastery
+    - elemental damage
+- character additional bonuses (from weapon, artifact, constellation, etc.)
+    - elemental bonus damages
+    - NA bonus damages
+    - CA bonus damages
+    - plunge bonus damages
+    - skill bonus damages
+    - burst bonus damages
+    - all damage bonuses
+
+## Outputs
+- minimun, maximum, and average damages for the below with no buffs
+    - Normal attacks
+    - Charge attacks
+    - Skill DMG
+    - Burst DMG
+- minimun, maximum, and average damages for the below with relevant elemental reactions
+    - Normal attacks
+    - Charge attacks
+    - Skill DMG
+    - Burst DMG
+
+## Planned Future Features
+- enemy level gap damage difference
+- enemy resistances
+- damage from lowering enemy defense and resistances
+- shield strength
+
+## How to update data
+- run `python getHoneyCharacterData.py`
+- update talent_categories.js file with new data
+    - If you don't, you may get incorrect calculations if it automatically categorizes the damage to the wrong talent
+- if a character has some sort of infusion or buff, make sure to add a new function to the calculate method since it isn't a normal calculation.
+
+Chongyun
---
+++ getHoneyCharacterData.py
@@ -0,0 +1,67 @@
+import requests
+import re
+
+base_url = 'https://genshin.honeyhunterworld.com/db/char/'
+page = requests.get(base_url + 'characters/?lang=EN').content.decode("utf-8") 
+pattern = r'<div class=char_sea_cont><a href="/db/char/(.*?)\/\?lang=EN"><img'
+characters = re.findall(pattern, page)
+
+with open('character_stats.js', 'w', encoding="utf-8") as file:
+    file.write('let data = [\n')
+    for character in characters:
+        if character == "feiyan":
+            file.write('"CHARACTER_NAME: yanfei",\n')
+        elif character == "shougun":
+            file.write('"CHARACTER_NAME: raiden",\n')
+        elif "traveler_boy" in character:
+            file.write(f'"CHARACTER_NAME: aether_{character[13:]}",\n')
+        elif "traveler_girl" in character:
+            file.write(f'"CHARACTER_NAME: lumine_{character[14:]}",\n')
+        else:
+            file.write('"CHARACTER_NAME: ' + character + '",\n')
+
+        page = requests.get(base_url + character).content
+        element = re.search(br'\/img\/icons\/element\/(.*?)_', page).group(1)
+        file.write(f'"CHARACTER_ELEMENT: {element.decode()}",\n')
+
+        element = re.search(br'Weapon Type<\/td><td><a href="\/db\/weapon\/(.*?)\/', page).group(1)
+        file.write(f'"CHARACTER_WEAPON: {element.decode()}",\n')
+
+        pattern = br'<table class=add_stat_table .*?<\/table>'
+        tables = [t for t in re.findall(pattern, page)]
+
+        talent = 1
+        if character in ["mona", "ayaka"]:
+            for table in tables[:2]:
+                pattern = br'<tr>.*?</tr>'
+                stats = [s for s in re.findall(pattern, table)]
+
+                for stat in stats:
+                    if b'Lv15' in stat:
+                        file.write(f'"TALENT_LEVEL: {talent}",\n')
+                        talent += 1
+                    else:
+                        file.write(f'"{stat.decode()}",\n')
+            # talent 4 because these characters have dash on talent 3
+            pattern = br'<tr>.*?</tr>'
+            stats = [s for s in re.findall(pattern, tables[3])]
+
+            for stat in stats:
+                if b'Lv15' in stat:
+                    file.write(f'"TALENT_LEVEL: {talent}",\n')
+                    talent += 1
+                else:
+                    file.write(f'"{stat.decode()}",\n')
+        else: 
+            for table in tables[:3]:
+                pattern = br'<tr>.*?</tr>'
+                stats = [s for s in re.findall(pattern, table)]
+
+                for stat in stats:
+                    if b'Lv15' in stat:
+                        file.write(f'"TALENT_LEVEL: {talent}",\n')
+                        talent += 1
+                    else:
+                        file.write(f'"{stat.decode()}",\n')
+        file.write('"",\n')
+    file.write('];')
